fb3eb1191dce207d03840e01b8831a7752f06f41
Ravien777 <r.sewpal@unasat.sr>
2024-08-09 21:46:57 -0300

    First commit

---
+++ includes/class-woo-masterkit-deactivator.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Woo_Masterkit
+ * @subpackage Woo_Masterkit/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woo_Masterkit
+ * @subpackage Woo_Masterkit/includes
+ * @author     Your Name <email@example.com>
+ */
+class Woo_Masterkit_Deactivator
+{
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {}
+}
